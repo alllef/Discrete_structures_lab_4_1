91c6d84cb209d2bf7a5d5f63333e63f9feec3d3b
alllef <alekz29@gmail.com>
2020-04-17 18:11:24 +0300

    final version where everything is working

---
+++ cmake-build-debug/CMakeFiles/Discrete_structures_lab_4_1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Discrete_structures_lab_4_1.dir/main.cpp.obj"
+  "Discrete_structures_lab_4_1.exe"
+  "Discrete_structures_lab_4_1.exe.manifest"
+  "Discrete_structures_lab_4_1.lib"
+  "Discrete_structures_lab_4_1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Discrete_structures_lab_4_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
